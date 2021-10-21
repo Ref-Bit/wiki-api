6fed102580d186b10d1d7e069723568f4b4bb642
Ref-Bit <rsv.gho@gmail.com>
2021-10-21 16:03:10 +0300

    Added dyk API

--- index.js
+++ index.js
@@ -5,9 +5,36 @@ const { StatusCodes } = require('http-status-codes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const BASE_URL =
+  'https://ar.wikipedia.org/wiki/%D8%A7%D9%84%D8%B5%D9%81%D8%AD%D8%A9_%D8%A7%D9%84%D8%B1%D8%A6%D9%8A%D8%B3%D9%8A%D8%A9';
 
 app.get('/', (req, res) => {
   res.send('<h1>Wiki API</h1>');
+});
+
+app.get('/dyk', async (req, res) => {
+  try {
+    const dykData = [];
+    const dykHTML = (await axios.get(BASE_URL)).data;
+    const $ = cheerio.load(dykHTML);
+
+    $('div.mp-dyk ul', dykHTML).each(function () {
+      const content = $(this).children().text().split('؟');
+      content
+        .filter(item => item)
+        .map(item => {
+          dykData.push({
+            info: item.replace(/\... /gm, ''),
+          });
+        });
+    });
+    return res.status(StatusCodes.OK).json(dykData);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send('<h5>Something went wrong. Please try again later.</h5>');
+  }
 });
 
 const start = () => {
