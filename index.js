85a81b16f08f0be34dfa78606157a7f87948ab0f
Ref-Bit <rsv.gho@gmail.com>
2021-10-25 15:38:51 +0300

    Added cors, helmet and rate limiter of 5 requests per minute

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const limiter = require('express-rate-limit')({
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+app.set('trust proxy', 1);
 
 app.use('/api/v1', require('./routes'));
 
